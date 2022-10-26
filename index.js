44e5723f1c9d5bdfe5a948379ddcbf74e9412b80
Leonardo Merza <ljmerza@gmail.com>
2022-10-26 12:49:53 -0400

    init

---
+++ index.js
@@ -0,0 +1,73 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const sideFence = [
+	'https://www.fast-growing-trees.com/products/black-mondo-grass',
+	'https://www.fast-growing-trees.com/products/sunshine-ligustrum-shrub',
+	'https://www.fast-growing-trees.com/products/orange-rocket-barberry-shrub',
+	'https://www.fast-growing-trees.com/products/crimson-fire-loropetalum',
+	'https://www.fast-growing-trees.com/products/ruby-loropetalum',
+	'https://www.fast-growing-trees.com/products/purple-diamond-loropetalum',
+	'https://www.fast-growing-trees.com/products/golden-euonymus-shrub',
+	'https://www.fast-growing-trees.com/products/emerald-n-gold-wintercreeper',
+	'https://www.fast-growing-trees.com/products/green-velvet-boxwood',
+	'https://www.fast-growing-trees.com/products/fire-power-nandina-shrub',
+	'https://www.fast-growing-trees.com/products/variegated-boxwood-shrub',
+	'https://www.fast-growing-trees.com/products/apollo-winterberry-holly',
+  'https://www.fast-growing-trees.com/products/crimson-pygmy-barberry',
+  'https://www.fast-growing-trees.com/products/forsythia',
+  'https://www.fast-growing-trees.com/products/enduring-summer-crape-myrtle',
+]
+
+const backFence = [
+  'https://www.fast-growing-trees.com/products/baby-giant-arborvitae',
+  'https://www.fast-growing-trees.com/products/berckmans-gold-arborvitae',
+  'https://www.fast-growing-trees.com/products/emerald-green-arborvitae',
+]
+
+const trees = [
+
+]
+
+
+async function scrapeData() {
+  const scrapedData = [];
+
+  for await(const url of sideFence) {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    const title = $('h1.product__title').children().remove().end().text();
+    const price = $('.product__price').text();
+
+    const product = {
+      title,
+      price,
+    }
+
+    $('.details__table > tbody > tr').each((i , row) => {
+      const rowText= $(row).text().replace(/\s\s+/g, '');
+      const [key, value] = rowText.split(':');
+
+      if(!['Does Not Ship To', 'Your Growing Zone', 'Grows Well In Zones', 'Botanical Name'].includes(key)) {
+        product[key] = value;
+      }
+    });
+
+    product.url = url;
+    scrapedData.push(product);
+  }
+
+  console.table(scrapedData);
+
+  fs.writeFile("scaped_data.json", JSON.stringify(scrapedData, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Successfully written data to file");
+  });
+}
+
+scrapeData();
